8a1f8ca34b54d4a44d4c3ac19a3193264016dfde
CookOS1803 <usovich5321@gmail.com>
2022-04-04 22:08:06 +0300

    try for wallrun

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -8,9 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float runningSpeed = 10f;
     public float jumpSpeed = 5f;
     public float mass = 20f;
+    public float wallContactRadius = 0.5f;
+    public Vector3 wallContactPosition;
+    public LayerMask wallContactLayer;
     private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
-    private bool wallContact = false;
+
+    public Vector3 RelativeContactPosition => transform.TransformPoint(wallContactPosition);
 
     void Start()
     {
@@ -19,32 +23,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (wallContact && !characterController.isGrounded)
+        var colls = Physics.OverlapSphere(RelativeContactPosition, wallContactRadius, wallContactLayer.value);
+
+        if (colls.Length != 0)
         {
-            moveDirection = transform.TransformDirection(Vector3.forward);
+            Debug.Log("ADADAD");
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * walkingSpeed;
+
+            moveDirection = forward + (colls[0].transform.position - transform.position);
         }
         else
         {
-            CalculateMovement();
-
-            if (Input.GetButton("Jump") && characterController.isGrounded)
-            {
-                moveDirection.y = jumpSpeed;
-            }
-
-            if (!characterController.isGrounded)
-            {
-                moveDirection.y -= mass * Time.deltaTime;
-            }
-            else if (moveDirection.y < 0f)
-            {
-                moveDirection.y = 0f;
-            }
+            CalculateMovementFromInputs();
+            CalculateGravity();
         }
+        
         characterController.Move(moveDirection * Time.deltaTime);
     }
 
-    private void CalculateMovement()
+
+    private void CalculateMovementFromInputs()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
@@ -56,10 +54,32 @@ public class PlayerMovement : MonoBehaviour
         float curSpeedY = speed * Input.GetAxisRaw("Horizontal");
 
         moveDirection = forward * curSpeedX + new Vector3(0f, moveDirection.y, 0f) + right * curSpeedY;
+        
+        if (Input.GetButton("Jump") && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+    }
+    
+    private void CalculateGravity()
+    {
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= mass * Time.deltaTime;
+        }
+        else if (moveDirection.y < 0f)
+        {
+            moveDirection.y = 0f;
+        }
     }
 
-    public void SetWallContact(bool state)
+    void OnDrawGizmos()
     {
-        wallContact = state;
+        Gizmos.color = Color.red;
+
+        Vector3 pos = transform.TransformPoint(wallContactPosition);
+        Gizmos.DrawWireSphere(pos, wallContactRadius);
+        pos.x *= -1f;
+        Gizmos.DrawWireSphere(pos, wallContactRadius);
     }
 }
