45b1cd8a6eae48595efe692c1beff14cde2be40f
CookOS1803 <usovich5321@gmail.com>
2022-04-05 11:54:32 +0300

    changed sphere overlap to linecast

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -8,13 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float runningSpeed = 10f;
     public float jumpSpeed = 5f;
     public float mass = 20f;
-    public float wallContactRadius = 0.5f;
-    public Vector3 wallContactPosition;
+    public float wallContactLength = 0.5f;
+    public Vector3 wallContactOrigin;
     public LayerMask wallContactLayer;
     private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
 
-    public Vector3 RelativeContactPosition => transform.TransformPoint(wallContactPosition);
+    public Vector3 RelativeContactOrigin => transform.TransformPoint(wallContactOrigin);
 
     void Start()
     {
@@ -23,14 +23,14 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        var colls = Physics.OverlapSphere(RelativeContactPosition, wallContactRadius, wallContactLayer.value);
+        RaycastHit hit;
 
-        if (colls.Length != 0)
+        if (Physics.Linecast(RelativeContactOrigin, RelativeContactOrigin + transform.TransformDirection(Vector3.left) * wallContactLength, out hit, wallContactLayer.value))
         {
-            Debug.Log("ADADAD");
-            Vector3 forward = transform.TransformDirection(Vector3.forward) * walkingSpeed;
-
-            moveDirection = forward + (colls[0].transform.position - transform.position);
+            Vector3 t = hit.point - RelativeContactOrigin;
+            //Euler
+            moveDirection = new Vector3(t.z, 0f, -t.x) * walkingSpeed + t;
+            
         }
         else
         {
@@ -77,9 +77,8 @@ public class PlayerMovement : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        Vector3 pos = transform.TransformPoint(wallContactPosition);
-        Gizmos.DrawWireSphere(pos, wallContactRadius);
-        pos.x *= -1f;
-        Gizmos.DrawWireSphere(pos, wallContactRadius);
+        Vector3 pos = RelativeContactOrigin;
+        Gizmos.DrawLine(pos, pos + transform.TransformDirection(Vector3.left) * wallContactLength);
+        Gizmos.DrawLine(pos, pos + transform.TransformDirection(Vector3.right) * wallContactLength);
     }
 }
