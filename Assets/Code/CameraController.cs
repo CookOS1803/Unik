4649faac5d8ea86f2da9a9d157a47dc889360c68
CookOS1803 <usovich5321@gmail.com>
2022-04-17 20:44:09 +0300

    basic movement

--- Assets/Code/CameraController.cs
+++ Assets/Code/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float smoothing = 1f;
+    [SerializeField] private float mouseFactor = 1f;
     private Vector3 offset;
 
     void Start()
@@ -15,7 +16,16 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 targetCamPos = target.position + offset;
-		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.x /= Screen.width;
+        mousePosition.y /= Screen.height;
+        mousePosition.z = mousePosition.y;
+        mousePosition.y = 0f;
+
+        mousePosition.x -= 0.5f;
+        mousePosition.z -= 0.5f;
+        
+        Vector3 targetCamPos = target.position + offset + mousePosition * mouseFactor;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
 }
--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -7,29 +5,93 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private LayerMask floorMask;
+    [SerializeField, Range(0f, 1f)] private float strafeDot = 0.85f;
     private CharacterController characterController;
+    private Animator animator;
+    private Vector3 moveDirection;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
 
-        Vector3 direction = new Vector3(h, 0f, v) * speed;
-        direction = Vector3.ClampMagnitude(direction, speed);
+        moveDirection = new Vector3(h, 0f, v);
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f);
 
-        characterController.Move(direction * Time.deltaTime);
+        characterController.Move(moveDirection * speed * Time.deltaTime);
+        
+        AnimateMovement(v, h);
+    }
 
+    private void AnimateMovement(float v, float h)
+    {
+        float dot = Vector3.Dot(transform.forward, moveDirection);
+
+        if (h == 0f && v == 0f)
+        {
+            animator.SetBool("IsMovingForward", false);
+            animator.SetBool("IsMovingBackwards", false);
+            animator.SetBool("IsMovingLeft", false);
+            animator.SetBool("IsMovingRight", false);
+        }
+        else if (Mathf.Abs(dot) < strafeDot)
+        {
+            bool isLeft = Vector3.SignedAngle(transform.forward, moveDirection, transform.up) < 0f;
+
+            animator.SetBool("IsMovingForward", false);
+            animator.SetBool("IsMovingBackwards", false);
+            animator.SetBool("IsMovingLeft", isLeft);
+            animator.SetBool("IsMovingRight", !isLeft);
+        }
+        else
+        {
+            bool isForward = dot > 0f;
+
+            animator.SetBool("IsMovingForward", isForward);
+            animator.SetBool("IsMovingBackwards", !isForward);
+            animator.SetBool("IsMovingLeft", false);
+            animator.SetBool("IsMovingRight", false);
+        }
+    }
+
+    private void Turn()
+    {
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit floorHit;
-		if (Physics.Raycast(camRay, out floorHit, Mathf.Infinity, floorMask.value))
-		{
-            Debug.Log(floorHit.point);
-            transform.LookAt(floorHit.point);
-		}
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, Mathf.Infinity, floorMask.value))
+        {
+            Vector3 hitPoint = floorHit.point;
+            hitPoint.y = transform.position.y;
+            transform.LookAt(hitPoint);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        float angle = Mathf.Rad2Deg * Mathf.Acos(strafeDot);
+
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, angle, 0f) * transform.forward);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, -angle, 0f) * transform.forward);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, 180f + angle, 0f) * transform.forward);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, 180f - angle, 0f) * transform.forward);
+
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawRay(transform.position, moveDirection);
     }
 }
