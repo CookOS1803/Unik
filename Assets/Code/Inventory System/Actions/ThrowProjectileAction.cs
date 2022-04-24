13b821447323b80916f385d27aeeb52cc3f0be76
CookOS1803 <usovich5321@gmail.com>
2022-04-24 13:22:26 +0300

    throw action

---
+++ Assets/Code/Inventory System/Actions/ThrowProjectileAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Item Actions/ThrowProjectileAction", fileName = "ThrowProjectileAction")]
+public class ThrowProjectileAction : ItemAction
+{
+    [SerializeField] private GameObject projectilePrefab;
+
+    public override void Use(Transform actor)
+    {
+        Instantiate(projectilePrefab, actor.position + Vector3.up + actor.forward, actor.rotation);
+    }
+}
